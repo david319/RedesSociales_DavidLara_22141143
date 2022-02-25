e5343f72b65576ba4a2055bb5a49ec9c443ca85d
david319 <larapineda2003@gmail.com>
2022-02-25 15:47:52 -0600

    Start

---
+++ src/main/java/com/example/redessociales/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.redessociales;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class HelloController {
+
+    public Button btn_log;
+    public TextField user;
+
+
+}
